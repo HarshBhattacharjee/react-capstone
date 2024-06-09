e095ff820eb11fd8952f1bc9004c56e60871e6c2
HarshBhattacharjee <harshb2626@gmail.com>
2024-06-09 17:56:46 +0530

    boilerplate react setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Form from './pages/Form'
+import Genre from './pages/Genre';
+import Info from './pages/Info';
+import Movies from './pages/Movies';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Form />} />
+      <Route path="/genre" element={<Genre />} />
+      <Route path="/info" element={<Info />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
